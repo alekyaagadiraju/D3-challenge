43ef94e0bf7232b7f26bdf562a4be808d8610f7e
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-05-25 20:13:47 -0400

    axis labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,22 +1,16 @@
 // D3 Scatter Plot
 
-// Width of box
+// Graph dimensions
 var width = parseInt(d3.select("#scatter").style("width"));
-
-// Height of the graph
 var height = width - width / 4.0;
-
-// Margin for graph
 var margin = 20;
-
-// Label Space
 var labelArea = 110;
 
 // padding for the text at the bottom and left axes
 var tPadBot = 40;
 var tPadLeft = 40;
 
-// Create the actual canvas for the graph
+// Canvas for graph
 var svg = d3
   .select("#scatter")
   .append("svg")
@@ -24,12 +18,11 @@ var svg = d3
   .attr("height", height)
   .attr("class", "chart");
 
-// Set the radius for each dot that will appear in the graph.
-// Note: Making this a function allows us to easily call
-// it in the mobility section of our code.
+// Radius for each dot
+
 var circRadius;
 function crGet() {
-  if (width <= 530) {
+  if (width <= 550) {
     circRadius = 5;
   }
   else {
@@ -38,19 +31,10 @@ function crGet() {
 }
 crGet();
 
-// The Labels for our Axes
-
-// A) Bottom Axis
-// ==============
-
-// We create a group element to nest our bottom axes labels.
+// Bottom axes labels
 svg.append("g").attr("class", "xText");
-// xText will allows us to select the group without excess code.
-var xText = d3.select(".xText");
 
-// We give xText a transform property that places it at the bottom of the chart.
-// By nesting this attribute in a function, we can easily change the location of the label group
-// whenever the width of the window changes.
+var xText = d3.select(".xText");
 function xTextRefresh() {
   xText.attr(
     "transform",
@@ -63,7 +47,23 @@ function xTextRefresh() {
 }
 xTextRefresh();
 
-// Now we use xText to append three text SVG files, with y coordinates specified to space out the values.
+// Left Axis
+var leftTextX = margin + tPadLeft;
+var leftTextY = (height + labelArea) / 2 - labelArea;
+
+svg.append("g").attr("class", "yText");
+
+var yText = d3.select(".yText");
+function yTextRefresh() {
+  yText.attr(
+    "transform",
+    "translate(" + leftTextX + ", " + leftTextY + ")rotate(-90)"
+  );
+}
+yTextRefresh();
+
+// Append SVG
+
 // 1. Poverty
 xText
   .append("text")
@@ -72,6 +72,7 @@ xText
   .attr("data-axis", "x")
   .attr("class", "aText active x")
   .text("In Poverty (%)");
+
 // 2. Age
 xText
   .append("text")
@@ -80,6 +81,7 @@ xText
   .attr("data-axis", "x")
   .attr("class", "aText inactive x")
   .text("Age (Median)");
+
 // 3. Income
 xText
   .append("text")
@@ -89,30 +91,7 @@ xText
   .attr("class", "aText inactive x")
   .text("Household Income (Median)");
 
-// B) Left Axis
-// ============
-
-// Specifying the variables like this allows us to make our transform attributes more readable.
-var leftTextX = margin + tPadLeft;
-var leftTextY = (height + labelArea) / 2 - labelArea;
-
-// We add a second label group, this time for the axis left of the chart.
-svg.append("g").attr("class", "yText");
-
-// yText will allows us to select the group without excess code.
-var yText = d3.select(".yText");
-
-// Like before, we nest the group's transform attr in a function
-// to make changing it on window change an easy operation.
-function yTextRefresh() {
-  yText.attr(
-    "transform",
-    "translate(" + leftTextX + ", " + leftTextY + ")rotate(-90)"
-  );
-}
-yTextRefresh();
-
-// Now we append the text.
+// Append the text
 // 1. Obesity
 yText
   .append("text")
@@ -140,11 +119,6 @@ yText
   .attr("class", "aText inactive y")
   .text("Lacks Healthcare (%)");
 
-// 2. Import our .csv file.
-// ========================
-// This data file includes state-by-state demographic data from the US Census
-// and measurements from health risks obtained
-// by the Behavioral Risk Factor Surveillance System.
 
 // Import our CSV data with d3's .csv import method.
 d3.csv("assets/data/data.csv").then(function(data) {
@@ -152,21 +126,15 @@ d3.csv("assets/data/data.csv").then(function(data) {
   visualize(data);
 });
 
-// 3. Create our visualization function
-// ====================================
-// We called a "visualize" function on the data obtained with d3's .csv method.
-// This function handles the visual manipulation of all elements dependent on the data.
+// Visualization function
+
 function visualize(theData) {
-  // PART 1: Essential Local Variables and Functions
-  // =================================
-  // curX and curY will determine what data gets represented in each axis.
-  // We designate our defaults here, which carry the same names
-  // as the headings in their matching .csv data file.
+
   var curX = "poverty";
   var curY = "obesity";
 
-  // We also save empty variables for our the min and max values of x and y.
-  // this will allow us to alter the values in functions and remove repetitious code.
+  // Empty variables for the min and max values of x and y.
+  
   var xMin;
   var xMax;
   var yMin;
